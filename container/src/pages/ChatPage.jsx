a50b97523ffb0597ab138fb2e515784c96d33682
rishabh.agarwal <rishabh.agarwal@nutanix.com>
2025-01-29 21:58:09 +0530

    Add basic code

--- container/src/pages/ChatPage.jsx
+++ container/src/pages/ChatPage.jsx
@@ -1,16 +1,59 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 
 import {
   FlexLayout,
+  DateIcon,
   StackingLayout,
-  Title,
-  Paragraph,
-  TextLabel
+  TextLabel,
+  Link,
+  Divider,
+  Button,
+  Loader,
 } from '@nutanix-ui/prism-reactjs';
 
-const ChatPage = () => {
+const ChatPage = ({ data }) => {
+  const [showLink, setShowLink] = useState(false);
+  const [showAddLoader, setShowAddLoader] = useState(false);
+
+  const generateLink = () => {
+    setShowAddLoader(true);
+    setTimeout(() => {
+      setShowAddLoader(false);
+      setShowLink(true);
+    }, 5000);
+  };
+
+  const openLink = () => {
+    window.open(data.link, '_blank');
+  };
+
+  const chatWithLogs = () => {
+    // Todo: show chat page
+  };
+
   return (
-    <div>Chat Page</div>
+    <div>
+      <StackingLayout
+        key={data.id}
+        style={{
+          width: '100vw',
+          height: '100vh',
+          border: 'none',
+          padding: '30px',
+          borderRadius: '0px',
+          margin: '0',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        itemSpacing="30px"
+      >
+        <TextLabel>
+          What can I help with?
+        </TextLabel>
+      </StackingLayout>
+    </div>
   );
 };
 
